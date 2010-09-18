ae7a550b9fa5a7268f37db68ab2c6c2d009b9b98
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-09-19 01:19:49 +0200

    mysql

--- src/geniusloci/settings.py
+++ src/geniusloci/settings.py
@@ -11,11 +11,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'geniusloci.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'geniusloci',                      # Or path to database file if using sqlite3.
+        'USER': 'geniusloci',                      # Not used with sqlite3.
+        'PASSWORD': 'geniusloci',                  # Not used with sqlite3.
+        'HOST': '10.211.10.116',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
