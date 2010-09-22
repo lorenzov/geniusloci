6a1986d83faf01fd794f6cab9e506604a8501710
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-09-22 15:21:10 +0200

    fb

--- src/geniusloci/facebook_oauth/context_processors.py
+++
@@ -1,8 +0,0 @@
-from facebook_oauth.models import FacebookUser
-
-def facebook(request):
-	try:
-		fb_user = FacebookUser.objects.get(user = request.user.id)
-	except FacebookUser.DoesNotExist:
-		fb_user = None
-	return { 'fb_user': fb_user }
--- src/geniusloci/facebook_oauth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from facebook_oauth.views import authenticate_view,logout_view,register
-
-urlpatterns = patterns('',
-    url(r'^authenticate/?$', view=authenticate_view,name='authenticate'),
-    url(r'^logout/?$', view=logout_view,name='logout'),
-    url(r'^register/?$', view=register,name='register'),
-)
