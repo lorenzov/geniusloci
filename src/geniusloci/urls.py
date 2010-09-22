a4ef6d24768d0bc1f31f851e16cc694ae7656278
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-09-22 14:30:15 +0200

    fb

--- src/geniusloci/urls.py
+++ src/geniusloci/urls.py
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
 	(r'', include('facebook_oauth.urls')),
+	(r'^login/$', 'geniusloci.loc.views.login'),
 	url(r'^$', 'geniusloci.loc.views.index'),
 	
 	
