d9a18423108db337c1726b72c40c5146e5d5fad8
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-09-22 15:42:25 +0200

    fb

--- src/geniusloci/loc/views.py
+++ src/geniusloci/loc/views.py
@@ -3,12 +3,12 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden, HttpResponseServerError, HttpResponsePermanentRedirect
 from django.shortcuts import render_to_response
 from django.template import Context, loader, RequestContext
-#from django.contrib.auth.decorators import login_required
-#from django.views.decorators.vary import vary_on_headers
-#from django.contrib.auth.models import *
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.vary import vary_on_headers
+from django.contrib.auth.models import *
 #from django.core.paginator import Paginator, InvalidPage, EmptyPage
 #from django.core import serializers
-#from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404
 #from django.core.cache import cache
 from geniusloci.loc.models import *
 import foursquare
