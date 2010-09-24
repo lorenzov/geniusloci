b6c23f73043f3bfdbff138e445dd4a73709fec24
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-09-24 13:34:35 +0200

    new geo

--- src/geniusloci/loc/views.py
+++ src/geniusloci/loc/views.py
@@ -188,7 +188,7 @@ def mobile_map(request, id):
 	
 	
 #500m = 0.33 miles ca
-def find_near(mylat, mylong, distance, distance_orig = 0):
+def find_near(mylat, mylong, distance, distance_orig = 0, null_foursquare_categ = False):
 	mylong = float(mylong)
 	mylat = float(mylat)
 	lon1 = mylong - distance/math.fabs(math.cos(math.radians(mylat))*69)
@@ -199,9 +199,9 @@ def find_near(mylat, mylong, distance, distance_orig = 0):
 	lat1= mylat - (distance/69)
 	lat2 = mylat+(distance/69)
 	
-	places = Place.objects.filter(geolong__gte = str(lon1), geolong__lte = str(lon2), geolat__gte = str(lat1), geolat__lte = str(lat2))
+	places = Place.objects.filter(geolong__gte = str(lon1), geolong__lte = str(lon2), geolat__gte = str(lat1), geolat__lte = str(lat2), null_foursquare_categ__isnull = False)
 	logging.debug(connection.queries[len(connection.queries) - 1])
-	if places.count() > 5:
+	if places.count() > 10:
 		return places
 	if distance_orig == 0:
 		distance_orig = distance
