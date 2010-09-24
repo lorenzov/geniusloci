8be4e0a275414471d6cc5fb5f5fc843ea93c132e
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-09-25 01:37:58 +0200

    browspe space

--- src/geniusloci/loc/browse_space.py
+++ src/geniusloci/loc/browse_space.py
@@ -7,7 +7,7 @@ lon = 9.2244
 
 
 for i in range(0,100):
-	client.get('/mobi/geo/?lat=' + str(lat)+ '&lon=' + str(lon))
+	c.get('/mobi/geo/?lat=' + str(lat)+ '&lon=' + str(lon))
 	lat += 0.0001
 	time.sleep(1)
 	print lat
