4ed910530601cdd82fe83eb6651c12d789cbd2bd
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-09-26 18:11:34 +0200

    browspe space

--- src/geniusloci/loc/browse_space.py
+++ src/geniusloci/loc/browse_space.py
@@ -3,12 +3,12 @@ import time
 import random
 
 c = Client()
-lat = 45.4560
+lat = 45.4530
 lon = 9.152
 
 
 for i in range(0,100):
 	c.get('/mobi/geo/?lat=' + str(lat)+ '&lon=' + str(lon))
 	lon += 0.00005
-	time.sleep(random.randint(0,10)/10)
+	time.sleep(random.randint(0,20)/10)
 	print lon
