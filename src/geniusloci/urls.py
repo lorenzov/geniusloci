b14b9df597dfc0a35dc3187ea74968658ea5d6a0
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-09-30 09:42:40 +0200

    mobile like enabled

--- src/geniusloci/urls.py
+++ src/geniusloci/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
 	
-	url(r'^$', 'geniusloci.loc.views.index'),
+	(r'^$', 'geniusloci.loc.views.index'),
 	
 	(r'^logout/$', 'geniusloci.loc.views.logout_view'),
 	(r'^p/(?P<slug>).*/(?P<id>\d+)/$', 'geniusloci.loc.views.place'),
