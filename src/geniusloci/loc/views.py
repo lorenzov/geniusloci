cfc18a9e5a3820346984fe33db0d66df6889b918
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-10-06 14:05:18 +0200

    mobile search for a place

--- src/geniusloci/loc/views.py
+++ src/geniusloci/loc/views.py
@@ -174,7 +174,7 @@ def geo(request):
 	places = []
 	if 's' in request.GET:
 		#filtering search by name
-		places = find_near(lat, lon, 0.30, mult_limit = 40)
+		places = find_near(lat, lon, 0.30, mult_limit = 60)
 		places = filter_places_by_name(places, request.GET['s'])
 	else:
 		places = find_near(lat, lon, 0.30)
