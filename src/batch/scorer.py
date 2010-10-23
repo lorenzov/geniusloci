2de4ededada8fd35026bd7c77cc9b6e1473bd3da
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-10-23 13:36:59 +0200

    ruler for calculating points

---
+++ src/batch/scorer.py
@@ -0,0 +1,26 @@
+from geniusloci.loc.models import *
+
+
+
+def score_like(like):
+	place = Place.objects.get(pk = like.place.id)
+	place.points = place.points + 1
+	place.save()
+	
+	
+def score_likes():
+	likes = Like.objects.all()
+	for like in likes:
+		score_like(like)
+	
+	
+def reset_points():
+	places = Place.objects.all()
+	for place in places:
+		place.points = 0
+		place.save()
+	pass	
+		
+if __name__ == '__main__':
+	reset_points()
+	score_places()
