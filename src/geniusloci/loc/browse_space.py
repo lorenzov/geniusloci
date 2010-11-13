2492d755c1c8ac490798eb5f49401797053069c1
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-11-13 09:37:17 +0100

    browse space

--- src/geniusloci/loc/browse_space.py
+++ src/geniusloci/loc/browse_space.py
@@ -8,7 +8,7 @@ lon = 9.13925
 
 
 for i in range(0,100):
-	c.get('/mobi/geo/?lat=' + str(lat)+ '&lon=' + str(lon))
+	c.get('/mobile/home/?lat=' + str(lat)+ '&lon=' + str(lon))
 	lon += 0.00005
 	time.sleep(random.randint(0,20)/10)
 	print lon
