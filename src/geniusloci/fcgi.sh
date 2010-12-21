c6b631e28880d5cff11d6a25207286eca733a966
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-12-21 15:27:32 +0100

    fcgi

--- src/geniusloci/fcgi.sh
+++ src/geniusloci/fcgi.sh
@@ -1,8 +1,8 @@
 # Replace these three settings.
-PROJDIR="/home/ubuntu/geniusloci/src/geniusloci"
+PROJDIR="/home/lviscanti/geniusloci/src/geniusloci"
 PIDFILE="$PROJDIR/mysite.pid"
-SOCKET="/home/ubuntu/geniusloci.sock"
-export PYTHONPATH=/home/ubuntu/geniusloci/src/:/var/django/
+SOCKET="/home/lviscanti/geniusloci.sock"
+export PYTHONPATH=/home/lviscanti/geniusloci/src/:/var/django/
 export DJANGO_SETTINGS_MODULE=geniusloci.settings
 cd $PROJDIR
 if [ -f $PIDFILE ]; then
