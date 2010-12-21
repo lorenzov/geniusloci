c9d25eda2f26bb3fff5fa708b050db3e10dd11f2
Lorenzo Viscanti <lorenzo@mikamai.com>
2010-12-21 11:37:37 +0100

    settings

--- src/geniusloci/settings.py
+++ src/geniusloci/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'geniusloci',                      # Or path to database file if using sqlite3.
         'USER': 'geniusloci',                      # Not used with sqlite3.
         'PASSWORD': 'geniusloci',                  # Not used with sqlite3.
-        'HOST': '10.211.10.116',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
